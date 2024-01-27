11070da48c88c58f622943087cd522d825d68a12
Harsh ranjan <67137451+harshra215@users.noreply.github.com>
2024-01-27 20:11:20 +0530

    Add files via upload

---
+++ components/Layouts/Footer.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  // Scroll State
+  const [isVisible, setIsVisible] = useState(false);
+  const scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const listenToScroll = () => {
+    let heightToHidden = 250;
+    const windowScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    windowScroll > heightToHidden ? setIsVisible(true) : setIsVisible(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+  });
+  return (
+    <>
+      <footer>
+        <Container>
+          <Row>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Location</h5>
+                <p>200 West Street</p>
+                <p>NewYork,NY,10282</p>
+                <p>United States</p>
+              </div>
+            </Col>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Working Hours</h5>
+                <p>Mon-Fri: 24x7</p>
+              </div>
+            </Col>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Order Now</h5>
+                <p>Quaerat neque purus ipsum</p>
+                <p>
+                  <Link to="tel:9322443517" className="calling">
+                    932-244-3517
+                  </Link>
+                </p>
+              </div>
+            </Col>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Follow Us</h5>
+                <p>Quaerat neque purus ipsum</p>
+                <ul className="list-unstyled text-center mt-2">
+                  <li>
+                    <Link to="/">
+                      <i class="bi bi-facebook"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">
+                      <i class="bi bi-twitter"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">
+                      <i class="bi bi-instagram"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">
+                      <i class="bi bi-youtube"></i>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+          <Row className="copy_right">
+            <Col>
+              <div>
+                <ul className="list-unstyled text-center mb-0">
+                  <li>
+                    <Link to="/">
+                      © 2024 <span>TastyBurger&Co</span>. All Rights Reserved
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">About Us</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Terms Of Use</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Privacy Policy</Link>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+
+      {/* Sroll To Top */}
+      {isVisible && (
+        <div className="scroll_top" onClick={scrollTop}>
+          <i class="bi bi-arrow-up"></i>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Footer;
